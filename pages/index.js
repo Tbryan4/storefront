61c9aea7dfeb4f085abf719588a3d77d08e330d0
Tylen Bryan <tylenbryan@gmail.com>
2022-04-10 16:46:22 -0600

    added changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {PageTitle} from "./../components/PageTitle"
 
 export default function Home() {
   return (
-    null
+    <>
+    <PageTitle title="storefront" tagline ="featured products"/> 
+    </>
   )
 }
